18f0a30ce5abf85f26cc2fb5f3cc9bfe93fcd777
Viacheslav <little.cado1@gmail.com>
2020-11-22 22:00:51 +0300

    Complete task 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MyDet} from './shared/models/det.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project11';
+
+  dets: MyDet[] = [];
+
+  inputId: number;
+  inputName: string;
+
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      this.dets.push(new MyDet(i, 'Датчик ' + i));
+    }
+  }
+
+  deleteDet(id: number) {
+    const elem = this.dets.find(det => det.id === id);
+    const elId: number = this.dets.indexOf(elem);
+
+    this.dets.splice(elId, 1);
+  }
+
+  addDet() {
+    this.dets.push(new MyDet(this.inputId, this.inputName));
+    this.inputId = undefined;
+    this.inputName = '';
+  }
+
 }
